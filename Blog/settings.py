63a5cca2a043b5e75aa3babf43465fef35b5a88a
Dharmesh <dharmeshsaini777@gmail.com>
2023-12-29 08:14:22 +0530

    B1

--- Blog/settings.py
+++ Blog/settings.py
@@ -134,9 +134,9 @@ import os
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
 
-STATICFILES_DIRS = {
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR , "public/static")
-}
+]
 
 MEDIA_ROOT = os.path.join(BASE_DIR , 'public/media')
 MEDIA_URL = '/media/'
